d0c57dd5182ca161a426272b57b16a952319a06a
MUEID11 <mehedirangpur3@gmail.com>
2024-04-15 20:08:24 +0600

    setting up project

---
+++ src/Routes/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div><button className="btn btn-secondary font-amrimo">HELLO</button></div>,
+    },
+  ]);
+
+  export default router;
